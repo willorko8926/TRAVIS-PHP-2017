4925b330a23bc6fde87206785a787c0f4db34457
willorko8926 <willianreyes52@hotmail.com>
2017-12-20 02:35:05 -0500

    hola

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<body style="background-color:grey; color:red">
 		<?php
-			echo '<p>Fernando Balladares</p>';
+			echo '<p>William Reyes	</p>';
 		?>
 	<a href="about.html">Acerca de</a>
 	</body>
